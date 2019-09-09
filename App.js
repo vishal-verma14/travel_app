094a1c92b4c2ddf3f7a2bdf62551299eb7b4ab65
vishal <vishal-verma14@users.noreply.github.com>
2019-09-10 00:12:13 +0530

    demo travel files

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Travel from "./navigation/Travel";
+import { createAppContainer } from "react-navigation";
+
+export default createAppContainer(Travel);
